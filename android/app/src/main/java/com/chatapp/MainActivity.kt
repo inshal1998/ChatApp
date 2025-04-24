93fd81914af39f2201d52154c0324844ad51cd8d
Inshal <inshalansari80@gmail.com>
2025-04-24 17:08:26 +0530

    GoogleSignIn and Package name refactor

--- android/app/src/main/java/com/chatapp/MainActivity.kt
+++ android/app/src/main/java/com/chatapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.chatapp
+package com.chatApp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/chatapp/MainApplication.kt
+++ android/app/src/main/java/com/chatapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.chatapp
+package com.chatApp
 
 import android.app.Application
 import com.facebook.react.PackageList
